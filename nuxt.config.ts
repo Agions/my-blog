8973146dcd9c424cc527a3a54f5638ac35140765
agions <1051736049@qq.com>
2022-11-24 18:20:59 +0800

    :tada: 初始化提交

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,6 @@ export default defineNuxtConfig({
         // <meta name="viewport" content="width=device-width, initial-scale=1">
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
-      script: [
-        {
-          src: "https://map.qq.com/api/gljs?v=1.exp&key=F76BZ-46ALF-FGFJC-JG4CY-WF3NQ-SJFYS&libraries=tools,service,drawing,geometry,autocomplete,convertor",
-          type: "text/javascript",
-          mode: "client",
-        },
-      ],
     },
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <script  setup >
-const initMap = () => {
-  console.log({ window })
-  if (document.getElementById('container')) {
-    let map = new TMap.Map(document.getElementById('container'), {
-      center: new TMap.LatLng(30.43758, 120.2817), //设置中心点坐标
-      zoom: 10,
-      viewMode: "2D",
-    });
-    console.log({ map })
-  }
 
-}
 onMounted(() => {
-  initMap()
 })
 
 </script>
